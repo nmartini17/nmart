d078efef58e406225a67d5168a747d22c6c7187b
Nicolai Martini <cph-nm172@cphbusiness.dk>
2022-05-05 00:09:22 +0200

    added favicon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./styles.css";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-import { MdEmail } from "react-icons/md";
+import { AiFillGithub, AiOutlineLinkedin } from "react-icons/ai";
+import { MdOutlineMail } from "react-icons/md";
+import { FaTrademark } from "react-icons/fa";
 import { IconContext } from "react-icons";
-import logo from "./Sinds-logo.png";
+import logo from "./no-bg.svg";
 
 export default function App() {
   return (
-    <div className='App'>
-      <a href='/'>
+    <div>
+      <a href='/' className='Logo'>
         <img
           src={logo}
+          class='LogoColor'
           alt='NM'
           width='62,5'
           height='35'
@@ -17,43 +19,52 @@ export default function App() {
         />
       </a>
 
-      <h1 className='Title'>Velkommen</h1>
-      <IconContext.Provider
-        value={{
-          color: "#043927",
-          className: "Icon",
-          size: "25px",
-          title: "Link to Github",
-        }}
-      >
-        <a href='https://github.com/nmartini17'>
-          <AiFillGithub />
-        </a>
-      </IconContext.Provider>
-      <IconContext.Provider
-        value={{
-          color: "#043927",
-          className: "Icon",
-          size: "25px",
-          title: "Link to Linkedin",
-        }}
-      >
-        <a href='https://www.linkedin.com/in/nicolaigmartini/'>
-          <AiFillLinkedin />
-        </a>
-      </IconContext.Provider>
-      <IconContext.Provider
-        value={{
-          color: "#043927",
-          className: "Icon",
-          size: "25px",
-          title: "Email",
-        }}
-      >
-        <a href='mailto:nicoulaisen@gmail.com'>
-          <MdEmail />
-        </a>
-      </IconContext.Provider>
+      <div className='TitleDiv'>
+        <h1>Velkommen</h1>
+      </div>
+
+      <div className='Icons'>
+        <IconContext.Provider
+          value={{
+            color: "#043927",
+            className: "Icon",
+            size: "50px",
+            title: "Link to Github",
+          }}
+        >
+          <a href='https://github.com/nmartini17'>
+            <AiFillGithub />
+          </a>
+        </IconContext.Provider>
+        <IconContext.Provider
+          value={{
+            color: "#043927",
+            className: "Icon",
+            size: "50px",
+            title: "Link to Linkedin",
+          }}
+        >
+          <a href='https://www.linkedin.com/in/nicolaigmartini/'>
+            <AiOutlineLinkedin />
+          </a>
+        </IconContext.Provider>
+        <IconContext.Provider
+          value={{
+            color: "#043927",
+            className: "Icon",
+            size: "50px",
+            title: "Email",
+          }}
+        >
+          <a href='mailto:nicoulaisen@gmail.com'>
+            <MdOutlineMail />
+          </a>
+        </IconContext.Provider>
+      </div>
+      <footer className='Footer'>
+        2022 - Nicolai Martini
+        <FaTrademark />
+      </footer>
     </div>
   );
 }
